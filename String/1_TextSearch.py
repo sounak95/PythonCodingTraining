3b99381b760a8edc6becaa7978cbba9a5d4fcb2d
Saha,Sounak <Sounak.Saha@misys.com>
2020-02-08 03:31:02 +0530

    updated

--- String/1_TextSearch.py
+++ String/1_TextSearch.py
@@ -6,13 +6,15 @@ check-string-substring-another
 def search(txt,pat):
     n=len(txt)
     m=len(pat)
-    for i in range(n-m):
+    for i in range(n-m+1):
+        flag = 0
         for j in range(m):
             if(txt[i+j]!=pat[j]):
+                flag=1
                 break
-        if(j==m-1):
+        if(flag==0):
             print("patter {} found at {}".format(pat,i))
 
-txt = "geeksforgeeks"
-pat = "for"
+txt = "BlUeBe1Bl*fjal9jkl"
+pat = "Bl"
 search(txt,pat)
